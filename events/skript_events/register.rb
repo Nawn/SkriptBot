10aed132765fde52b8f35747dd68fc4ce991187d
Raul Gonzalez <Nawn1994@gmail.com>
2017-09-17 22:49:41 -0700

    Skript now adds User. Must implement Ease of use

--- events/skript_events/register.rb
+++ events/skript_events/register.rb
@@ -5,13 +5,7 @@ module SkriptEvents
 	  command :register  do |event, user|
       discord_user = event.bot.parse_mention(user)
   		if discord_user.is_a?(Discordrb::User)
-        input_hash = {
-          "User Name" => "#{discord_user.username}",
-          "User ID" => "#{discord_user.id}",
-          "Skript URL" => "#{Skript::Bot.website_url}"
-        }
-
-  			next Skript::Bot.debug_hash(input_hash)
+        Skript::Bot.new_user(discord_user)
   		else
         next Skript::Bot.argument_error("User is invalid")
   		end
--- lib/skript_bot.rb
+++ lib/skript_bot.rb
@@ -1,12 +1,12 @@
+require "rest-client"
+
 module Skript
 	class Bot
-		@website_url = "http://localhost:3000"
-		class << self
-			attr_accessor :website_url
-		end
+		@website_url = "http://192.168.1.18:3000"
+		@api_key = "test"
 
-		def initialize(args)
-				
+		class << self
+			attr_accessor :website_url, :api_key
 		end
 
 		def self.argument_error(input_string)
@@ -23,6 +23,32 @@ module Skript
 			return_string += "```"
 
 			return return_string
+		end
+
+		def self.new_user(discord_user)
+
+			query_params = {
+				"bot_key" => "#{Skript::Bot.api_key}",
+				"discord_id" => "#{discord_user.id}",
+				"discord_nick" => "#{discord_user.username}"
+			}
+
+			# Base URL for a new user
+			query_url = "#{Skript::Bot.website_url}/user/new?"
+
+			# For each param, add it as a URL Param
+			query_params.each do |key, value|
+				query_url += "#{key}=#{value}&"
+			end
+
+			# Because of the previous bit (Iteration on the params)
+			# The last one should be an & character.
+			# So, remove it. 
+			query_url[-1] = ""
+
+			response = RestClient.get(query_url)
+
+			Skript::Bot.debug_hash({"response" => response})
 		end
 	end
 end
