f97df7b1bcac8305df3f99434bd8be1c169b60fa
Raul Gonzalez <Nawn1994@gmail.com>
2017-09-24 10:37:53 -0700

    Respond with temporary

--- events/skript_events/register.rb
+++ events/skript_events/register.rb
@@ -5,7 +5,8 @@ module SkriptEvents
 	  command :register  do |event, user|
       discord_user = event.bot.parse_mention(user)
   		if discord_user.is_a?(Discordrb::User)
-        Skript::Bot.new_user(discord_user)
+        Skript::Bot.new_user(discord_user, event)
+        next
   		else
         next Skript::Bot.argument_error("User is invalid")
   		end
